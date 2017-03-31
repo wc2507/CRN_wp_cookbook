f1597ec901bdb7e78734ebac7fa49584526f50f0
Wei Cai <wc2507@columbia.edu>
2017-03-30 23:14:25 -0400

    updated cookbook

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -14,10 +14,10 @@
  * @package WordPress
  */
 
-define('WP_REDIS_HOST', '');
+define('WP_REDIS_HOST', 'wordpress.e6dowe.0001.usw2.cache.amazonaws.com');
  
-define('WP_HOME','');
-define('WP_SITEURL','');
+define('WP_HOME','http://wordpress-1998380490.us-west-2.elb.amazonaws.com');
+define('WP_SITEURL','http://wordpress-1998380490.us-west-2.elb.amazonaws.com');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
