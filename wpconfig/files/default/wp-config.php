c70a306f3ab33950e7ceb7bda473829846e0d797
Wei Cai <wc2507@columbia.edu>
2017-08-29 03:47:46 -0400

    Update wp-config.php

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -16,8 +16,8 @@
 
 define('WP_REDIS_HOST', 'wordpress.e6dowe.0001.usw2.cache.amazonaws.com');
  
-define('WP_HOME','dgw16o4xk60o3.cloudfront.net');
-define('WP_SITEURL','dgw16o4xk60o3.cloudfront.net');
+define('WP_HOME','http://dgw16o4xk60o3.cloudfront.net');
+define('WP_SITEURL','http://dgw16o4xk60o3.cloudfront.net');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
