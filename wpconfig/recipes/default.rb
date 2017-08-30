9ad93ba1d168e8cc7ba6caff13ddfaaf7c5c5366
Wei Cai <wc2507@columbia.edu>
2017-08-30 02:27:18 -0400

    Update default.rb

--- wpconfig/recipes/default.rb
+++ wpconfig/recipes/default.rb
@@ -1,4 +1,4 @@
-cookbook_file "/srv/www/wordpress/current/wp-config.php" do
+cookbook_file "/srv/www/crn_wordpress/current/wp-config.php" do
   source "wp-config.php"
   mode '0755'
   action :create
